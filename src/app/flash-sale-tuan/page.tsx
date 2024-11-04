dd79b46a259f878f7b3652efc1500aa92e4c233e
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-11-04 15:56:55 +0700

    update

--- src/app/flash-sale-tuan/page.tsx
+++ src/app/flash-sale-tuan/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Banner from "../../components/flash-sale-tuan/banner/index";
 import ProductList from "../../components/flash-sale-tuan/product/index";
 import AppleList from "../../components/flash-sale-tuan/apple/index";
@@ -16,8 +16,94 @@ import ic1 from "../../../public/flase-sale/imagefl1.png";
 import ic2 from "../../../public/flase-sale/img2.png";
 import background from "../../../public/flase-sale/5be4e162eaa8a.webp";
 import Image from "next/image";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+const categories = [
+  { id: "item-hot", name: "Giá sốc" },
+  { id: "item-iphone", name: "iPhone" },
+  { id: "item-airpods", name: "Máy 99%" },
+  { id: "item-ipad", name: "iPad" },
+  { id: "item-mac", name: "Laptop" },
+  { id: "item-watch", name: "Android" },
+  { id: "item-toy", name: "Phụ kiện Apple" },
+];
 function Page() {
+  const categoryRef = useRef(null);
+  const swiperRef = useRef<any>(null);
+  const [isStickyVisible, setIsStickyVisible] = useState(false);
+  const [activeCategory, setActiveCategory] = useState<string | null>(null);
+  const scrollThreshold = 500;
+
+  const handleClick = (id: string, offset = 0) => {
+    const element = document.getElementById(id);
+    if (element) {
+      const elementPosition =
+        element.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+      setActiveCategory(id);
+    }
+  };
+
+  const handleScrollToRules = () => {
+    const customOffset = 500;
+    handleClick("item-rules", customOffset);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsStickyVisible(window.scrollY > scrollThreshold);
+    };
+
+    const sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveCategory(entry.target.id);
+          }
+        });
+      },
+      { root: null, threshold: 0.1 }
+    );
+
+    const observeSections = () => {
+      categories.forEach((category, index) => {
+        const element = document.getElementById(category.id);
+        if (element) {
+          sectionObserver.observe(element);
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    const timeoutId = setTimeout(observeSections, 0);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearTimeout(timeoutId);
+      categories.forEach((category) => {
+        const element = document.getElementById(category.id);
+        if (element) {
+          sectionObserver.unobserve(element);
+        }
+      });
+    };
+  }, []);
+
+  useEffect(() => {
+    if (swiperRef.current) {
+      const activeIndex = categories.findIndex(
+        (category) => category.id === activeCategory
+      );
+      if (activeIndex !== -1) {
+        swiperRef.current.slideTo(activeIndex, 300, true);
+      }
+    }
+  }, [activeCategory]);
   return (
     <div className="page-flase-sale-tuan">
       <Image
@@ -56,6 +142,66 @@ function Page() {
       </div>
       <div id="item-rules">
         <Rules />
+      </div>
+      <div
+        className={`sticky-category ${isStickyVisible ? "visible" : "hidden"}`}
+      >
+        <div className="category-desktop">
+          {categories.map((category, index) => (
+            <div
+              key={index}
+              className={`category-item ${
+                activeCategory === category.id ? "active" : "default"
+              }`}
+              onClick={() => handleClick(category.id)}
+            >
+              <span className="category-name">{category.name}</span>
+            </div>
+          ))}
+        </div>
+        <div className="category-mobile">
+          <Swiper
+            slideToClickedSlide={true}
+            spaceBetween={10}
+            watchSlidesProgress={true}
+            onSwiper={(swiperInstance) => {
+              swiperRef.current = swiperInstance; // Store swiper instance in ref
+            }}
+            onSlideChange={(swiperInstance) => {
+              setActiveCategory(categories[swiperInstance.activeIndex].id);
+              swiperInstance.slideTo(swiperInstance.activeIndex, 300, true); // Center the active slide when scrolling
+            }}
+            breakpoints={{
+              300: {
+                slidesPerView: 3.5,
+              },
+              850: {
+                slidesPerView: 5,
+              },
+            }}
+            slidesPerView="auto"
+            initialSlide={0}
+          >
+            {categories.map((category, index) => (
+              <SwiperSlide
+                key={index}
+                onClick={() => {
+                  setActiveCategory(category.id);
+                  swiperRef.current?.slideTo(index, 300, true); // Center the clicked slide
+                  handleClick(category.id);
+                }}
+              >
+                <div
+                  className={`swiper-slide ${
+                    activeCategory === category.id ? "active" : "default"
+                  }`}
+                >
+                  <span className="category-name">{category.name}</span>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
       </div>
     </div>
   );
