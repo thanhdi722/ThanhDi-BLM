3f7fb8f3189a868f0d8f7e86c4e7d309f769eaf0
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-11-04 16:54:48 +0700

    update

--- src/app/Halloween/page.tsx
+++ src/app/Halloween/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import HeaderHalloween from "../../components/ComponentPageHalloween/HeaderHalloween/HeaderHalloween";
 import BodyHallowween from "../../components/ComponentPageHalloween/BodyHalloween/BodyHalloween";
 import ProductList from "../../components/ComponentPageHalloween/product/index";
@@ -9,96 +8,10 @@ import LaptopList from "../../components/ComponentPageHalloween/laptop/index";
 import ToyList from "../../components/ComponentPageHalloween/toy/index";
 import Rules from "../../components/ComponentPageHalloween/rules/index";
 import BannerHalloween from "../../components/ComponentPageHalloween/BannerHalloween/page";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import "./halloween.scss";
-const categories = [
-  { id: "item-hot", name: "Giá sốc" },
-  { id: "item-iphone", name: "iPhone" },
-  { id: "item-airpods", name: "Máy 99%" },
-  { id: "item-ipad", name: "iPad" },
-  { id: "item-mac", name: "Laptop" },
-  { id: "item-watch", name: "Android" },
-  { id: "item-toy", name: "Phụ kiện Apple" },
-];
+
 export default function page() {
-  const categoryRef = useRef(null);
-  const swiperRef = useRef<any>(null);
-  const [isStickyVisible, setIsStickyVisible] = useState(false);
-  const [activeCategory, setActiveCategory] = useState<string | null>(null);
-  const scrollThreshold = 500;
-
-  const handleClick = (id: string, offset = 0) => {
-    const element = document.getElementById(id);
-    if (element) {
-      const elementPosition =
-        element.getBoundingClientRect().top + window.pageYOffset;
-      const offsetPosition = elementPosition - offset;
-
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
-      setActiveCategory(id);
-    }
-  };
-
-  const handleScrollToRules = () => {
-    const customOffset = 500;
-    handleClick("item-rules", customOffset);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsStickyVisible(window.scrollY > scrollThreshold);
-    };
-
-    const sectionObserver = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setActiveCategory(entry.target.id);
-          }
-        });
-      },
-      { root: null, threshold: 0.1 }
-    );
-
-    const observeSections = () => {
-      categories.forEach((category, index) => {
-        const element = document.getElementById(category.id);
-        if (element) {
-          sectionObserver.observe(element);
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    const timeoutId = setTimeout(observeSections, 0);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-      clearTimeout(timeoutId);
-      categories.forEach((category) => {
-        const element = document.getElementById(category.id);
-        if (element) {
-          sectionObserver.unobserve(element);
-        }
-      });
-    };
-  }, []);
-
-  useEffect(() => {
-    if (swiperRef.current) {
-      const activeIndex = categories.findIndex(
-        (category) => category.id === activeCategory
-      );
-      if (activeIndex !== -1) {
-        swiperRef.current.slideTo(activeIndex, 300, true);
-      }
-    }
-  }, [activeCategory]);
   return (
     <div className="halloween">
       <svg
@@ -175,66 +88,6 @@ export default function page() {
 
       <div id="item-rules">
         <Rules />
-      </div>
-      <div
-        className={`sticky-category ${isStickyVisible ? "visible" : "hidden"}`}
-      >
-        <div className="category-desktop">
-          {categories.map((category, index) => (
-            <div
-              key={index}
-              className={`category-item ${
-                activeCategory === category.id ? "active" : "default"
-              }`}
-              onClick={() => handleClick(category.id)}
-            >
-              <span className="category-name">{category.name}</span>
-            </div>
-          ))}
-        </div>
-        <div className="category-mobile">
-          <Swiper
-            slideToClickedSlide={true}
-            spaceBetween={10}
-            watchSlidesProgress={true}
-            onSwiper={(swiperInstance) => {
-              swiperRef.current = swiperInstance; // Store swiper instance in ref
-            }}
-            onSlideChange={(swiperInstance) => {
-              setActiveCategory(categories[swiperInstance.activeIndex].id);
-              swiperInstance.slideTo(swiperInstance.activeIndex, 300, true); // Center the active slide when scrolling
-            }}
-            breakpoints={{
-              300: {
-                slidesPerView: 3.5,
-              },
-              850: {
-                slidesPerView: 5,
-              },
-            }}
-            slidesPerView="auto"
-            initialSlide={0}
-          >
-            {categories.map((category, index) => (
-              <SwiperSlide
-                key={index}
-                onClick={() => {
-                  setActiveCategory(category.id);
-                  swiperRef.current?.slideTo(index, 300, true); // Center the clicked slide
-                  handleClick(category.id);
-                }}
-              >
-                <div
-                  className={`swiper-slide ${
-                    activeCategory === category.id ? "active" : "default"
-                  }`}
-                >
-                  <span className="category-name">{category.name}</span>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
       </div>
     </div>
   );
