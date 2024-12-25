898e86a13567996c51b1b001363372b38c4c3fe4
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-12-25 08:30:21 +0700

    uuuu

--- src/components/ComponentSaleNewYear/banner/index.tsx
+++ src/components/ComponentSaleNewYear/banner/index.tsx
@@ -1,84 +1,90 @@
-'use client'
-import React, { useState, useEffect } from 'react'
-import Image from 'next/image'
-import './style.scss'
-import { Spin } from 'antd'
-import Link from 'next/link'
+"use client";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+import "./style.scss";
+import { Spin } from "antd";
+import Link from "next/link";
 
 function HeaderHalloween() {
-  const [endDate, setEndDate] = useState(new Date('2024-12-24T21:30:00'))
+  const [endDate, setEndDate] = useState(new Date("2024-12-29T21:30:00"));
   const [timeArray, setTimeArray] = useState([
     { date: endDate.toDateString(), days: 0, hours: 0, minutes: 0, seconds: 0 },
-  ])
-  const [isEventOver, setIsEventOver] = useState(false)
+  ]);
+  const [isEventOver, setIsEventOver] = useState(false);
   interface BannerItem {
-    banner_id: number
-    caption: string
-    link: string
-    media: string
-    media_alt: string
-    name: string
-    slider_id: number
+    banner_id: number;
+    caption: string;
+    link: string;
+    media: string;
+    media_alt: string;
+    name: string;
+    slider_id: number;
   }
 
   interface Banner {
-    __typename: string
-    items: BannerItem[]
+    __typename: string;
+    items: BannerItem[];
     page_info: {
-      current_page: number
-      page_size: number
-      total_pages: number
-    }
+      current_page: number;
+      page_size: number;
+      total_pages: number;
+    };
   }
 
   interface SliderItem {
-    title: string
-    identifier: string
-    Banner: Banner
+    title: string;
+    identifier: string;
+    Banner: Banner;
   }
 
   interface SliderData {
     Slider: {
-      items: SliderItem[]
-      total_count: number
-    }
+      items: SliderItem[];
+      total_count: number;
+    };
   }
 
   interface ApiResponse {
-    data: SliderData
+    data: SliderData;
   }
   useEffect(() => {
     const interval = setInterval(() => {
-      const now = new Date()
-      const timeDiff = endDate.getTime() - now.getTime()
+      const now = new Date();
+      const timeDiff = endDate.getTime() - now.getTime();
 
       if (timeDiff <= 0) {
-        setIsEventOver(true)
-        clearInterval(interval)
-        return
+        setIsEventOver(true);
+        clearInterval(interval);
+        return;
       }
 
-      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24))
-      const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60))
-      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000)
+      const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
 
-      setTimeArray([{ date: endDate.toDateString(), days, hours, minutes, seconds }])
-    }, 1000)
+      setTimeArray([
+        { date: endDate.toDateString(), days, hours, minutes, seconds },
+      ]);
+    }, 1000);
 
-    return () => clearInterval(interval)
-  }, [endDate])
-  const [data, setData] = useState<ApiResponse | null>(null)
+    return () => clearInterval(interval);
+  }, [endDate]);
+  const [data, setData] = useState<ApiResponse | null>(null);
 
   const fetchBannerHeader = async () => {
     try {
-      const response = await fetch('https://beta-api.bachlongmobile.com/graphql', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          query: `
+      const response = await fetch(
+        "https://beta-api.bachlongmobile.com/graphql",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            query: `
             query getSlider($filter: SliderFilterInput) {
               Slider(filter: $filter) {
                 items {
@@ -94,25 +100,26 @@ function HeaderHalloween() {
               }
             }
           `,
-          variables: {
-            filter: {
-              identifier: {
-                eq: 'don-giang-sinh-24-12',
+            variables: {
+              filter: {
+                identifier: {
+                  eq: "don-giang-sinh-24-12",
+                },
               },
             },
-          },
-        }),
-      })
+          }),
+        }
+      );
 
-      const result = await response.json()
-      setData(result)
+      const result = await response.json();
+      setData(result);
     } catch (err) {
-      console.error('Error fetching data', err)
+      console.error("Error fetching data", err);
     }
-  }
+  };
   useEffect(() => {
-    fetchBannerHeader()
-  }, [])
+    fetchBannerHeader();
+  }, []);
 
   return (
     <div className="page-sale-thang-12">
@@ -146,7 +153,7 @@ function HeaderHalloween() {
       </div>
       <div
         className="banner-HeaderHalloween shine-banner"
-        style={{ position: 'relative', overflow: 'hidden' }}
+        style={{ position: "relative", overflow: "hidden" }}
       ></div>
       <div className="container">
         {isEventOver ? (
@@ -154,13 +161,13 @@ function HeaderHalloween() {
             <p
               className="HeaderHalloween-time-line-end-text"
               style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                padding: '40px 0px',
-                color: '#ff000e',
-                fontSize: '32px',
-                fontWeight: '600',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "40px 0px",
+                color: "#ff000e",
+                fontSize: "32px",
+                fontWeight: "600",
               }}
             >
               Hết thời gian sự kiện
@@ -169,7 +176,9 @@ function HeaderHalloween() {
         ) : (
           <div className="header-deal-24-12">
             <h1>
-              <div className="newYear-deal-24-12">{/* Giáng sinh an lành - nhận quà hạnh phúc */}</div>
+              <div className="newYear-deal-24-12">
+                {/* Giáng sinh an lành - nhận quà hạnh phúc */}
+              </div>
               <div className="title-deal-24-12">thời gian còn lại</div>
             </h1>
             <h2 className="countdown-deal-24-12">
@@ -182,25 +191,38 @@ function HeaderHalloween() {
         )}
         <div
           style={{
-            padding: '10px',
+            padding: "10px",
             // background: "linear-gradient(0deg, #0002ff, #7490ff)",
             // borderRadius: "10px",
-            marginBottom: '20px',
+            marginBottom: "20px",
           }}
         >
-          <div className="HeaderHalloween-promotion-header" style={{ fontWeight: 400 }}>
+          <div
+            className="HeaderHalloween-promotion-header"
+            style={{ fontWeight: 400 }}
+          >
             {` `}
-            <p className="glitch-24-12">8 đặc quyền mua hàng tại Bạch Long Mobile</p>
+            <p className="glitch-24-12">
+              8 đặc quyền mua hàng tại Bạch Long Mobile
+            </p>
           </div>
           <div className="HeaderHalloween-promotion-list-privilege">
             {data?.data?.Slider?.items[0]?.Banner?.items
-              .filter((item) => item.name.includes('ưu đãi đón giáng sinh'))
+              .filter((item) => item.name.includes("ưu đãi đón giáng sinh"))
               .map((item, index) => (
-                <div key={index} className="privilege-img" style={{ cursor: 'pointer' }}>
+                <div
+                  key={index}
+                  className="privilege-img"
+                  style={{ cursor: "pointer" }}
+                >
                   {item.link ? (
-                    <a href={item.link} target="_blank" rel="noopener noreferrer">
+                    <a
+                      href={item.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       <Image
-                        src={item.media || ''}
+                        src={item.media || ""}
                         alt={`privilege-${index + 1}`} // Adjust the alt text accordingly
                         width={1200}
                         height={1000}
@@ -208,7 +230,7 @@ function HeaderHalloween() {
                     </a>
                   ) : (
                     <Image
-                      src={item.media || ''}
+                      src={item.media || ""}
                       alt={`privilege-${index + 1}`} // Adjust the alt text accordingly
                       width={1200}
                       height={1000}
@@ -220,7 +242,7 @@ function HeaderHalloween() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default HeaderHalloween
+export default HeaderHalloween;
