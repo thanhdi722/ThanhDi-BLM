de3c43262e4de5649782d0fba4c9a5e6e993c590
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-12-31 11:36:58 +0700

    eqweqwqwe

--- src/app/anh-vu-dep-trai/page.tsx
+++ src/app/anh-vu-dep-trai/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 
-export default function page() {
+export default function Page() {
   const [dataTitle, setDataTitle] = useState<any>(null);
   const [dataProducts, setDataProducts] = useState<any>({});
   const fetchBannerHeader = async () => {
