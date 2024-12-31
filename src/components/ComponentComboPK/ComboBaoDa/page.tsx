6fd0f9248db58f73e032eb0f8fc72fd14086a5e1
DESKTOP-FECONH7\THANH DI <thanhdi721@gmail.com>
2024-12-31 11:26:18 +0700

    ádasdas

--- src/components/ComponentComboPK/ComboBaoDa/page.tsx
+++ src/components/ComponentComboPK/ComboBaoDa/page.tsx
@@ -1,94 +1,94 @@
-/* eslint-disable @next/next/no-img-element */
-"use client";
-import React, { useState, useEffect } from "react";
-import "./style.scss";
-import CardProduct from "../CardProductComboPK/CardProduct";
-import { Spin } from "antd";
-import "swiper/css";
-import "swiper/css/navigation";
-import { useProductComboBaoDa } from "../hook/ComboPK/ComboBaoDa/ComboBaoDa";
+// /* eslint-disable @next/next/no-img-element */
+// "use client";
+// import React, { useState, useEffect } from "react";
+// import "./style.scss";
+// import CardProduct from "../CardProductComboPK/CardProduct";
+// import { Spin } from "antd";
+// import "swiper/css";
+// import "swiper/css/navigation";
+// import { useProductComboBaoDa } from "../hook/ComboPK/ComboBaoDa/ComboBaoDa";
 
-const SectionBaoDa: React.FC = () => {
-  const [dataTitle, setDataTitle] = useState<any>(null);
-  const { data } = useProductComboBaoDa();
-  console.log("data check apple ", data);
-  const fetchBannerHeader = async () => {
-    try {
-      const response = await fetch(
-        "https://beta-api.bachlongmobile.com/graphql",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            query: `
-                  query getSlider($filter: SliderFilterInput) {
-                    Slider(filter: $filter) {
-                      items {
-                      title
-                        Banner {
-                          __typename
-                          items {
+// const SectionBaoDa: React.FC = () => {
+//   const [dataTitle, setDataTitle] = useState<any>(null);
+//   const { data } = useProductComboBaoDa();
+//   console.log("data check apple ", data);
+//   const fetchBannerHeader = async () => {
+//     try {
+//       const response = await fetch(
+//         "https://beta-api.bachlongmobile.com/graphql",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify({
+//             query: `
+//                   query getSlider($filter: SliderFilterInput) {
+//                     Slider(filter: $filter) {
+//                       items {
+//                       title
+//                         Banner {
+//                           __typename
+//                           items {
 
-                            link
-                            media
-                            media_alt
-                            name
+//                             link
+//                             media
+//                             media_alt
+//                             name
 
-                          }
+//                           }
 
-                        }
-                      }
+//                         }
+//                       }
 
-                    }
-                  }
-                `,
-            variables: {
-              filter: {
-                identifier: {
-                  eq: "banner-page-combo-phu-kien",
-                },
-              },
-            },
-          }),
-        }
-      );
+//                     }
+//                   }
+//                 `,
+//             variables: {
+//               filter: {
+//                 identifier: {
+//                   eq: "banner-page-combo-phu-kien",
+//                 },
+//               },
+//             },
+//           }),
+//         }
+//       );
 
-      const result = await response.json();
-      setDataTitle(result);
-    } catch (err) {}
-  };
-  useEffect(() => {
-    fetchBannerHeader();
-  }, []);
-  return (
-    <div className="OldForNew-Section-leather-case" id="item-leather-case">
-      <div className="container">
-        <div className="OldForNew-Section-Container-leather-case">
-          {dataTitle ? (
-            dataTitle?.data?.Slider?.items[0]?.Banner?.items
-              .filter((item: any) =>
-                item.name.includes("title bao da trang phụ kiện")
-              )
-              .map((item: any, index: any) => (
-                <div key={index}>
-                  <img
-                    src={item.media || ""}
-                    alt={`privilege-${index + 1}`}
-                    style={{ padding: "0px 10px 20px 10px" }}
-                  />
-                </div>
-              ))
-          ) : (
-            <Spin>
-              <div style={{ width: 200, height: 200 }} />
-            </Spin>
-          )}
-        </div>
-      </div>
-    </div>
-  );
-};
+//       const result = await response.json();
+//       setDataTitle(result);
+//     } catch (err) {}
+//   };
+//   useEffect(() => {
+//     fetchBannerHeader();
+//   }, []);
+//   return (
+//     <div className="OldForNew-Section-leather-case" id="item-leather-case">
+//       <div className="container">
+//         <div className="OldForNew-Section-Container-leather-case">
+//           {dataTitle ? (
+//             dataTitle?.data?.Slider?.items[0]?.Banner?.items
+//               .filter((item: any) =>
+//                 item.name.includes("title bao da trang phụ kiện")
+//               )
+//               .map((item: any, index: any) => (
+//                 <div key={index}>
+//                   <img
+//                     src={item.media || ""}
+//                     alt={`privilege-${index + 1}`}
+//                     style={{ padding: "0px 10px 20px 10px" }}
+//                   />
+//                 </div>
+//               ))
+//           ) : (
+//             <Spin>
+//               <div style={{ width: 200, height: 200 }} />
+//             </Spin>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default SectionBaoDa;
+// export default SectionBaoDa;
